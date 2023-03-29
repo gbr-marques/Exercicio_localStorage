1aa3f864e33508015869d5c6590f82f02eb84874
Gabriel Marques Silva <x384890@rede.sp>
2023-03-29 17:01:37 -0300

    adicionando validações e botão de deletar a lista

--- Mochila de Viagem/mochila-de-viagem/js/main.js
+++ Mochila de Viagem/mochila-de-viagem/js/main.js
@@ -1,6 +1,7 @@
 const cadastrar = document.getElementById('novoItem');
 const lista = document.getElementById("lista");
-const itens = JSON.parse(localStorage.getItem("itens")) || [];
+let itens = JSON.parse(localStorage.getItem("itens")) || [];
+let i = 0;
 
 // Função que resgata os elementos existentes no localStorage através do forEach
 
@@ -13,36 +14,48 @@ itens.forEach(e => {
 cadastrar.addEventListener ("submit", (evento) => {
     evento.preventDefault();
 
-    const item = cadastrar.elements['nome'];
-    const quantidade = cadastrar.elements['quantidade'];
+    const item = cadastrar.elements['nome'].value;
+    const quantidade = cadastrar.elements['quantidade'].value;
+    const nome_item = cadastrar.elements['nome'].value.trim(); // Função que elimina espaços localizados no início e fim da string
 
-    const itemAtual = {
-        "nome": item.value,
-        "quantidade": quantidade.value
-    }
-
-    // Condição que define se o nome do item já existe na lista
-
-    const existe = itens.find( e => e.nome === item.value)
-
-    if (existe) {
-
-        itemAtual.id = existe.id
-        atualizaElemento(itemAtual);
-        itens[existe.id] = itemAtual;
-
-    } else {
-
-        itemAtual.id = itens.length
-        criarElemento (itemAtual)
-        itens.push(itemAtual);
-
-    }
-
-    item.value = "";
-    quantidade.value = ""; 
-
-    localStorage.setItem("itens", JSON.stringify(itens));
+    // if (item == "" || quantidade == "") {
+    //     alert("Os campos 'Nome do item' e 'Quantidade' devem ser preenchidos")
+    // } else {
+    
+        const itemAtual = {
+            "nome": nome_item,
+            "quantidade": quantidade,
+            "id": i
+        }
+    
+        // Condição que define se o nome do item já existe na lista
+    
+        //const identificador = itens.find( e => e.nome === nome_item)
+        //|| (e.nome).toLowerCase() === (nome_item).toLowerCase() || ( e => e.nome === nome_item && (e.nome).toLowerCase() === (nome_item).toLowerCase())
+        //const repete1 = itens.find(e => e.nome === nome_item)
+        const repete2 = itens.find(e => (e.nome).toLowerCase() === (nome_item).toLowerCase())
+        console.log(repete2);
+        console.log(quantidade);
+        
+        //existe.nome.localeCompare()
+        //console.log("O id do item repetido é " + identificador.id + " e a sua quantidade atual é " +identificador.quantidade + ", quando deveria");
+    
+        if (repete2) {
+                console.log(repete2.id);
+                itemAtual.id = repete2.id
+                atualizaElemento(itemAtual);
+                itens[repete2.id].quantidade = quantidade;
+        } else {
+            criarElemento (itemAtual)
+            itens.push(itemAtual);
+        }
+    
+        item.value = "";
+        quantidade.value = ""; 
+    
+        localStorage.setItem("itens", JSON.stringify(itens));
+            
+    // }
 
 }) 
 
@@ -63,7 +76,8 @@ function criarElemento(item) {
     info_item.innerHTML += item.nome;
     
     novoItem.appendChild(info_item);
-    novoItem.appendChild(botaoDeleta());
+    novoItem.appendChild(botaoDeleta(item.id));
+    i++
 
     lista.appendChild(novoItem); //inserindo o elemento li na lista (tag ul)
 }
@@ -76,14 +90,40 @@ function atualizaElemento (item) {
 
 //Função para criar um botão de deletar item
 
-function botaoDeleta() {
+function botaoDeleta(id) {
     const botao = document.createElement('button');
     botao.classList.add("botao_deleta");
     botao.innerHTML = "X";
    
     botao.addEventListener("click", function() {
-        deletaElemento(this);
+        deletaElemento(this.parentNode, id);
     })
 
     return botao;
 }
+
+//Função que deleta o elemento selecionado
+
+function deletaElemento(tag, id) {
+    tag.remove();
+    const id_remocao = itens.findIndex(elemento => elemento.id === id);
+    itens.splice (id_remocao, 1);
+    localStorage.setItem("itens", JSON.stringify(itens));
+}
+
+// Função que limpa os itens da lista
+
+const botao_limpa = document.getElementById("limpa");
+botao_limpa.addEventListener("click", () => {
+    const todos_os_itens = document.querySelectorAll("[class='item'");
+
+    console.log("Botão pressionado")
+    for (let i = 0; i < (itens.length); i++) {
+        todos_os_itens[i].remove();
+    }
+
+    console.log(itens);
+    itens = []
+    console.log(itens);
+    localStorage.clear();
+})
